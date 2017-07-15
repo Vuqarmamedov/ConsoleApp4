2f1f23e486cac2405aef899e3105b424ba619778
Vuqarmamedov <vuqar.mamedovv@gmail.com>
2017-07-15 19:50:53 +0400

    Add project files.

---
+++ ConsoleApp4/Home.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Home :ISize,IgetInfo
+    {
+        public int [] eniUzunu { get; set; }
+        public int rooms { get; set; }
+        public Window []  pencere { get; set; }
+        public Door [] qapi { get; set; }
+
+        public bool enterOrNoEnter()
+        {
+            for (int i = 0; i < qapi.Length; i++)
+            {
+                if (qapi[i].openClose)
+                {
+
+                    return true;
+                    
+                }
+            }
+            return false;
+        }
+    }
+
+
+    public class Door :ISize,IgetInfo
+    {
+       public int [] eniUzunu { get; set; }
+       public bool iron { get; set; }
+        public bool openClose { get; set; }
+    }
+
+    public class Window :ISize,IgetInfo
+    {
+       public int [] eniUzunu { get; set; }
+       public bool plastik { get; set; }
+       public int lays { get; set; }
+       public string glassType { get; set; }
+
+        public Glass shushe { get; set; }
+        
+    }
+    public class Glass 
+    {
+        public int qalinliqi { get; set; }
+        public string marka { get; set; }
+        public string reng { get; set; }
+
+    }
+
+      public interface ISize
+    {
+        int[] eniUzunu { get; set; }
+    }
+
+    interface IgetInfo
+    {
+        
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home Ev = new Home();
+            Ev.eniUzunu = new int[2] { 3600, 2500 };
+            Ev.rooms =  4;
+            
+            
+            Ev.pencere = new Window[3] {
+                new Window
+                {
+                    eniUzunu = new int [2] {120,220},
+                    lays = 25,
+                    plastik = true,
+                    glassType = "temperli",
+                    shushe=new Glass
+                    {
+                        qalinliqi= 30,
+                        marka = "bomba marka",
+                        reng = "ag"
+                    }
+                },
+                new Window
+                {
+                    eniUzunu =new int [2] {130,240},
+                    lays=10,
+                    plastik = true,
+                    glassType = "temperli",
+                },
+                new Window
+                {
+                    eniUzunu =new int [2] {130,240},
+                    lays=10,
+                    plastik = false,
+                    glassType = "temperli",
+                }
+            };
+
+            Ev.qapi = new Door[2] {
+                new Door
+                {
+                    eniUzunu =new int [2] {1200,2200},
+                    iron = false,
+                    openClose = false
+                    
+                },
+                new Door
+                {
+                    eniUzunu =new int [2] {1300,2400},
+                    iron = true,
+                    openClose= false
+                }
+            };
+            Console.WriteLine("netice" + Ev.enterOrNoEnter());
+
+        }
+    }
+}
